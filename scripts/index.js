16af06a8dc62f6e506a304c3015f15702f3d9cf5
Oscar Jorge Castillo <oscarjorgecastillo@gmail.com>
2018-06-06 18:13:36 +0200

    Subo todo

---
+++ scripts/index.js
@@ -0,0 +1,118 @@
+
+$(document).ready(function () {
+    $('body').scrollspy({ target: '#navbar-scrollspy' })
+    new WOW().init();
+
+    // Add smooth scrolling to all links in navbar + footer link
+    $(".navbar a, footer a[href='#myPage']").on('click', function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+    function animate() {
+        $(".slideanim").each(function () {
+            var pos = $(this).offset().top;
+
+            var winTop = $(window).scrollTop();
+            if (pos < winTop + 800) {
+                $(this).css("visibility", "visible");
+
+                $(this).addClass($(this).closest(".bloque").data("animatedclass"));
+            }
+
+
+        });
+    }
+    animate();
+    $(window).scroll(function () {
+        animate();
+    });
+    $(".rotate").click(function () {
+        $(this).toggleClass("down");
+        $(this).closest('.bloque-servicio').find('.contenido-subbloque').toggle({
+            duration: 700
+        });
+    });
+
+    $("form").validate({
+        rules: {
+            nombre: {
+                required: true
+            },
+            mail: {
+                required: true,
+                email: true
+            },
+            asunto: {
+                required: true
+            },
+            mensaje: {
+                required: true
+            }
+        },
+        errorPlacement: function (error, element) {
+            // Don't show error
+        }
+    });
+});
+
+function sendMail(nombre, mail, asunto, mensaje) {
+    
+
+    if ($("form").valid()) {
+        $('#btnEnviar>i').toggleClass('fa-paper-plane-o fa-spinner fa-pulse');
+        $('#sobre').toggleClass('fa-envelope fa-envelope-open');
+        var parametros = {
+            "nombre": nombre,
+            "mail": mail,
+            "asunto": asunto,
+            "mensaje": mensaje,
+        };
+        $.ajax({
+            data: parametros,
+            url: 'send.php',
+            type: 'POST',
+
+            success: function (response) {
+                $('#btnEnviar').removeClass('btn-danger');
+                $("form")[0].reset();
+                $("i.prefix, label").removeClass('active');
+                var options =  {
+                    content: "El mensaje se ha enviado correctamente",
+                }
+                $.snackbar(options);
+                console.log(response);
+            },
+            error:function(){
+                $('#btnEnviar').addClass('btn-danger'); 
+                var options =  {
+                    content: "Ha ocurrido un error y no se ha podido enviar el mensaje.",
+                }
+                $.snackbar(options);
+            },
+            complete:function(){
+                setTimeout(function(){ 
+                    $('#btnEnviar>i').toggleClass('fa-paper-plane-o fa-spinner fa-pulse');
+                    $('#sobre').toggleClass('fa-envelope fa-envelope-open');
+                }, 1500);
+            }
+        });
+
+    }
+
+}
---
+++ send.php
@@ -0,0 +1,43 @@
+
+<?php 
+echo("success");
+//echo '0';
+ //require_once 'lib/swift_required.php';
+ //echo '1';
+// $transport = Swift_MailTransport::newInstance();
+//  $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+//    ->setUsername('oscarjorgecastillo@gmail.com')
+//    ->setPassword('Nerea22052012');
+   //echo '2';
+// $mailer = Swift_Mailer::newInstance($transport);
+// echo '3';
+// $message = Swift_Message::newInstance('Test Subject')
+//   ->setFrom(array('abc@example.com' => 'ABC'))
+//   ->setTo(array('xyz@test.com'))
+//   ->setBody('This is a test mail.');
+// echo '4';
+// $result = $mailer->send($message);
+// echo $result;
+
+
+
+
+
+
+
+
+// $to      = 'info@lubara.es';
+// $subject = 'Contacto web.Asunto: '+ $_POST['asunto'];
+// $message = 'Una persona con nombre '+ $_POST['nombre']+' y mail'+$_POST['mail']+' se ha puesto en contacto con Lubara a través de la página web. ' + $_POST['mensaje'];
+// // $headers = 'From: oscarjorgecastillo@gmail.com' . "\r\n" .
+// //     'Reply-To: oscarjorgecastillo@gmail.com' . "\r\n" .
+// //     'X-Mailer: PHP/' . phpversion();
+
+// if(mail($to, $subject, $message, $headers)){
+//     echo("success");
+// }
+// else{
+//     echo("error");
+// }
+
+?>
